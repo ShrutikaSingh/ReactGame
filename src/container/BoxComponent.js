e55875f226a7635220e01f1faa852ca1d2416893
ShrutikaSingh <shrutika051220@gmail.com>
2020-07-25 20:52:05 +0530

    update box with active and inactive states

--- src/container/BoxComponent.js
+++ src/container/BoxComponent.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-
+import "./BoxComponent.css";
 const BoxComponent = () => {
     const [boxState, setboxState] = useState({
         activeBox: {},
@@ -7,11 +7,25 @@ const BoxComponent = () => {
           { id: 1, top: 1 , zindex: 10, display: "block"},
         ],
       });
+
+
+      const toggleActive = (index) => {
+        setboxState({ ...boxState, activeBox: boxState.objects[index] });
+      };
+    
+      const toggleActiveStyles = (index) => {
+        if (boxState.objects[index] === boxState.activeBox) {
+          return "box active";
+        } else {
+          return "box inactive";
+        }
+      };
+
       const increaseObjects = (index) => {
         console.log(boxState.objects.concat({ id: index }), "obj");
          setboxState({
            ...boxState,
-           objects: boxState.objects.concat({ id: index, top: 1, zindex: index+20, display: "block" }),
+           objects: boxState.objects.concat({zindex: index+20 }),
          });
        };
     return(
@@ -29,7 +43,9 @@ const BoxComponent = () => {
           {boxState.objects.map((elements, index) => (
             <div
               key={index}
-              style={{ top: `${ boxState.objects[index].top }px`, zIndex: boxState.objects[index].zindex,  position: "relative", display: boxState.objects[index].display }}
+              className={toggleActiveStyles(index)}
+              style={{ top: `${ boxState.objects[index].top }px`, zIndex: boxState.objects[index].zindex,  position: "relative", display: boxState.objects[index].display, left: `${ boxState.objects[index].left }px`}}
+              onClick={() => toggleActive(index)}
             >
               {boxState.objects[index].top}
               <br></br>
