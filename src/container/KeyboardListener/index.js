d1c81fc733364d66b1f9fba7a97cc5509e259662
ShrutikaSingh <shrutika051220@gmail.com>
2020-07-25 23:52:28 +0530

    update keyBoardLister to handiling different keys

--- src/container/KeyboardListener/index.js
+++ src/container/KeyboardListener/index.js
@@ -1,23 +1,68 @@
-import { Component } from "react";
+import  {Component} from "react";
+import {KeyCodes} from "./keyCodes";
+import PropTypes from "prop-types";
 
-class KeyboardListener extends Component{
-    constructor(props){
-        super();
-        console.log("at keyboard listner")
+class KeyBoardListner extends Component{
+  
+    static propTypes = {
+        onKeyDown: PropTypes.func.isRequired
     }
-    //adding event listener for every component re-renders
     componentDidMount(){
-        document.addEventListener("keydown");
+        document.addEventListener('keydown', this.handleKeyDown);
+    }
+    componentWillUnmount(){
+        document.removeEventListener('keydown', this.handleKeyDown);
     }
 
-    componentWillMount(){
-        document.addEventListener("keydown");
-    }
+    onKeyDown = (keyCode) => {
+        const {handleGo}=this.props  //getting the handleGo function from BoxComponent
+        // eslint-disable-next-line default-case
+        switch (keyCode) {
+            case KeyCodes.ARROW_UP:
+                handleGo(30, 0);
+                break;
+            case KeyCodes.W_UP:
+                handleGo(30, 0);
+                break;
+            case KeyCodes.ARROW_DOWN:
+                handleGo(-30, 0);
+                break;
+            case KeyCodes.S_DOWN:
+                handleGo(-30, 0);
+                break;
+            case KeyCodes.ARROW_LEFT:
+                handleGo(0, 30);
+                break;
+            case KeyCodes.A_LEFT:
+                handleGo(0, 30);
+                break;
+            case KeyCodes.ARROW_RIGHT:
+                handleGo(0, -30);
+                break;
+            case KeyCodes.D_RIGHT:
+                handleGo(0, -30);
+                break;
+            case KeyCodes.DELETE:
+                handleGo(0,0,"none")
+      }
+    };
 
+    //function for checking the keyCodes and then executing the keyDown func
+    handleKeyDown = event => {
+        event.preventDefault()
+        const {keyCode} = event;
+        //console.log(event,"at event")
+        const allowedKeyCodes = Object.values(KeyCodes)
+        if(allowedKeyCodes.includes(keyCode)){
+            this.onKeyDown(keyCode)
+        }
+        return;
+    }
     render(){
-    return this.props.children
+        //console.log("eer",this.props.handleGo)
+        return this.props.children
     }
-    
 }
 
-export default KeyboardListener;
+export default  KeyBoardListner ;
+
