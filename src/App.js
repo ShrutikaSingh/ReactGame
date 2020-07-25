f125092280c3db7d57a2cc0bda3009fd6ba33465
ShrutikaSingh <shrutika051220@gmail.com>
2020-07-25 19:46:27 +0530

    add a simple boxComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import BoxComponent from "./container/BoxComponent"
 
 function App() {
   return (
     <div className="App">
      <h1>Hello World</h1>
+      <BoxComponent/>
     </div>
   );
 }
