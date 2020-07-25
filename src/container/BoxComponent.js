6083cc66b816f94bdc845c194bf5ea9c6f64cad5
ShrutikaSingh <shrutika051220@gmail.com>
2020-07-25 22:07:03 +0530

    add functions to move the keys

--- src/container/BoxComponent.js
+++ src/container/BoxComponent.js
@@ -1,36 +1,103 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./BoxComponent.css";
+import { KeyCodes } from "./KeyboardListener/keyCodes";
+
 const BoxComponent = () => {
-    const [boxState, setboxState] = useState({
-        activeBox: {},
-        objects: [
-          { id: 1, top: 1 , zindex: 10, display: "block"},
-        ],
-      });
+  const [boxState, setboxState] = useState({
+    activeBox: {},
+    objects: [
+      { id: 1, top: 1 , zindex: 10, display: "block"},
+    ],
+  });
+  const toggleActive = (index) => {
+    setboxState({ ...boxState, activeBox: boxState.objects[index] });
+  };
+  const toggleActiveStyles = (index) => {
+    if (boxState.objects[index] === boxState.activeBox) {
+      return "box active";
+    } else {
+      return "box inactive";
+    }
+  };
+
+  const increaseObjects = (index) => {
+   // console.log(boxState.objects.concat({ id: index }), "obj");
+    setboxState({
+      ...boxState,
+      objects: boxState.objects.concat({ id: index, top: 1, zindex: index+20, display: "block" }),
+    });
+  };
+
+  const handleKeyDown = (keyCode) => {
+      console.log("w_up",KeyCodes.ARROW_UP || KeyCodes.W_UP)
+    // eslint-disable-next-line default-case
+    switch (keyCode) {
+        case KeyCodes.ARROW_UP:
+            handleGoUp();
+            break;
+        case KeyCodes.W_UP:
+            handleGoUp();
+            break;
+        case KeyCodes.ARROW_DOWN:
+            handleGoDown();
+            break;
+        case KeyCodes.KeyCodes.S_DOWN:
+            handleGoDown();
+            break;
+        case KeyCodes.DELETE:
+            handleDelete()
+    }
+  };
+  const handleGoUp = () => {
+    console.log("Up key pressed", boxState.activeBox); //{id: 4, margins: "40px"}
+    // setboxState(prevStyle => ({
+    //     ...prevStyle,
+    //     activeBox: { ...prevStyle.activeBox, margins: "200px"}
+    // }));
+    // setboxState
+    const boxIndex = boxState.objects.findIndex(
+      (i) => i.id === boxState.activeBox.id
+    );
+    console.log("handleGoDown -> boxIndex", boxState.activeBox);
+
+    let newBoxData = boxState;
+    newBoxData.objects[boxIndex] = {
+      ...boxState.activeBox,
+      top: boxState.activeBox.top-30,
+    };
+    setboxState({ ...newBoxData });
+  };
+
+  const handleGoDown = () => {
+    //console.log("down key pressed", boxState.activeBox); //{id: 4, top: "40px"}
+    const boxIndex = boxState.objects.findIndex(
+      (i) => i.id === boxState.activeBox.id
+    );
+    let newBoxData = boxState;
+    newBoxData.objects[boxIndex] = {
+      ...boxState.activeBox,
+      top: boxState.activeBox.top+30,
+    };
+    setboxState({ ...newBoxData });
+  };
+
+  const handleDelete = () => {
+    //console.log("delete key pressed", boxState.activeBox); //{id: 4, top: "40px"}
+    const boxIndex = boxState.objects.findIndex(
+      (i) => i.id === boxState.activeBox.id
+    );
+    let newBoxData = boxState;
+    newBoxData.objects[boxIndex] = {
+      ...boxState.activeBox,
+      display: "none",
+    };
+    setboxState({ ...newBoxData });
+
+  };
 
 
-      const toggleActive = (index) => {
-        setboxState({ ...boxState, activeBox: boxState.objects[index] });
-      };
-    
-      const toggleActiveStyles = (index) => {
-        if (boxState.objects[index] === boxState.activeBox) {
-          return "box active";
-        } else {
-          return "box inactive";
-        }
-      };
-
-      const increaseObjects = (index) => {
-        console.log(boxState.objects.concat({ id: index }), "obj");
-         setboxState({
-           ...boxState,
-           objects: boxState.objects.concat({zindex: index+20 }),
-         });
-       };
-    return(
-        <>
-        Box Component
+  return (
+    <>
         <div className="inputbutton">
           <button
             type="submit"
@@ -44,7 +111,7 @@ const BoxComponent = () => {
             <div
               key={index}
               className={toggleActiveStyles(index)}
-              style={{ top: `${ boxState.objects[index].top }px`, zIndex: boxState.objects[index].zindex,  position: "relative", display: boxState.objects[index].display, left: `${ boxState.objects[index].left }px`}}
+              style={{ top: `${ boxState.objects[index].top }px`, zIndex: boxState.objects[index].zindex,  position: "relative", display: boxState.objects[index].display }}
               onClick={() => toggleActive(index)}
             >
               {boxState.objects[index].top}
@@ -53,8 +120,8 @@ const BoxComponent = () => {
             </div>
           ))}
         </div>
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default BoxComponent;
